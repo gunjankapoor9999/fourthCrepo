029afa3069bac4cc5b39086a9add3f34af537996
abc <abc@gmail.com>
2020-05-12 18:17:28 -0700

    fourth commit

---
+++ challenge11.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a='a';
+while(a<='z')
+{
+printf("%c\n",a);
+a+=1;
+}
+return 0;
+}
---
+++ challenge13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n=1;
+while(n<=100){
+   if(n%2!=0)
+      printf("%d\n",n);
+  n+=1;
+      }
+return 0;
+}
